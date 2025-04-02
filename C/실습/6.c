ccbdca25c6d4f4c2218d23a4224afd4b389ff000
BBIYAKYEE7 <bbiyakyee7@gmail.com>
2025-04-02 22:52:25 +0900

    Code Update

---
+++ C/실습/6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int input;
+    scanf("%d", &input);
+
+    int num = 1;
+    int sum = 0;
+
+    for(int i = 1; i <= input, i++;){
+        for(int j = 1; j <= i; j++) {
+            sum += num;
+            printf("%d ", num);
+        }
+    printf("\n");
+    } 
+}
